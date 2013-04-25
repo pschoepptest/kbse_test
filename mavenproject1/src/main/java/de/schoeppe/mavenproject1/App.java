4a86982a10b16bea054e227e137e2da22d772097
janaw <jwillmann91@hotmail.com>
2013-04-25 13:40:38 +0200

    jana commitet

--- mavenproject1/src/main/java/de/schoeppe/mavenproject1/App.java
+++ mavenproject1/src/main/java/de/schoeppe/mavenproject1/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello WorldWorld! feature2" );
+        System.out.println( "Hello WorldWorld! feature2 jana" );
     }
 }
