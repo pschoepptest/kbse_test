8ee600b745a876281cc9e2f13cc26c47a56ef50a
Philip <Philip@philips-macbook.speedport_w723_v_typ_a_1_00_096>
2013-04-25 13:51:31 +0200

    philip geschrieben neu klasse

---
+++ mavenproject1/src/main/java/de/schoeppe/mavenproject1/NeueKlasse.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.schoeppe.mavenproject1;
+
+/**
+ *
+ * @author Philip
+ */
+public class NeueKlasse {
+    
+}
